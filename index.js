11cf6974cdf8e95e2cf5b200a962912c782bc7ef
Logan Merchant <merchantclogan@gmail.com>
2020-10-16 01:52:42 -0600

    add role queries, getEmployees query

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const Department = require('./lib/departmentQueries');
+const Role = require('./lib/roleQueries');
+const Employee = require('./lib/employeeQueries');
 const department = new Department;
+const role = new Role;
+const employee = new Employee;
+
+employee.getEmployees();
