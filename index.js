155db33292af677de17498747b6e06ef1d2c5101
Logan Merchant <merchantclogan@gmail.com>
2020-10-17 23:12:23 -0600

    add getDepartmentId in role, getManagerNames in employee

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 const makeQuery = require('./lib/prompts');
 
 makeQuery();
-
-// const Department = require('./lib/departmentQueries');
-
-// const department = new Department;
-
-// department.getDepartmentNames();
